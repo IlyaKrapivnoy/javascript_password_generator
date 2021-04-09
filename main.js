b91024df6fa97d16536195ba96a26934262a94fb
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-09 15:26:10 +0300

    getting random numbers

--- main.js
+++ main.js
@@ -8,4 +8,8 @@ function getRandomUppercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
-console.log(getRandomUppercase());
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+console.log(getRandomNumber());
