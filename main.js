1387c7e7b03ae8e0d6b887d81f9f94aed33aef24
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-09 15:51:42 +0300

    Filtering out unchecked types

--- main.js
+++ main.js
@@ -15,6 +15,50 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
+// Generate event listen
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber= numbersEl.checked;
+    const hasSymbols = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbols, 
+        length
+    )
+})
+
+// Generate password func
+function generatePassword(lower, upper, number, symbol, length) {
+    // 1. Init password variable
+    // 2. Filter out unchecked types
+    // 3. Loop over length call generator func for each type
+    // 4. Add final password to the password var and return
+
+    let generatedPassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    console.log('typesCount: ', typesCount)
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+    (
+        item => Object.values(item)[0]
+    );
+
+    console.log('typesArr: ', typesArr);
+
+    if(typesCount === 0) {
+        return '';
+    }
+
+    
+}
+
 // Generator functions 
 
 function getRandomLower() {
