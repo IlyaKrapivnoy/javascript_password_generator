f09f0217ada2e824d64f634652b458639563ed89
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-09 15:24:08 +0300

    getting uppercase chars for password

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(getRandomLower());
+function getRandomUppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+console.log(getRandomUppercase());
